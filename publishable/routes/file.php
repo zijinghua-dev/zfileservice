e5562a271b75e2b6eb9e732392e529876b8b5827
root <root@DESKTOP-PIN5QEJ.localdomain>
2020-09-18 15:27:31 +0800

    所有请求都改为post

--- publishable/routes/file.php
+++ publishable/routes/file.php
@@ -2,9 +2,9 @@
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::group(['prefix' => 'file',], function () {
-        Route::post('/', 'FileController@upload');
-        Route::get('/{md5?}', 'FileController@show');
-        Route::delete('/{md5}', 'FileController@delete');
+        Route::post('/store', 'FileController@upload');
+        Route::post('/show', 'FileController@show');
+        Route::post('/delete', 'FileController@delete');
     });
     Route::group(['prefix' => 'config',], function () {
         Route::post('/', 'ConfigController@index');
